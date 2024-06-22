38e66e75c69a78db74fb9bd74ab6f6f0b4862503
parpace <parpace@gmail.com>
2024-06-21 21:33:54 -0500

    update UserProfile.jsx, update server.js and PostController.js to include getPostsByUserId

--- react-front-end/src/components/UserProfile.jsx
+++ react-front-end/src/components/UserProfile.jsx
@@ -1,35 +1,80 @@
 import { useContext, useEffect, useState } from 'react'
 import Header from './Header'
 import { useParams } from 'react-router-dom'
+import LoggedInUserContext from './LoggedInUserContext'
 
 export default function UserProfile () {
   
-  // const [posts, setPosts] = useState([])
-  // const [postComments, setPostComments] = useState([])
-  // const { userId } = useParams
-  // const { loggedInUser, setLoggedInUser } = useContext
+  const [viewedUser, setViewedUser] = useState({})
+  const [posts, setPosts] = useState([])
+  const [postComments, setPostComments] = useState([])
+  const { userId } = useParams()
+  const { loggedInUser } = useContext(LoggedInUserContext)
+  const [postText, setPostText] = useState('')
 
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   const getUser = async () => {
-  //     const userResponse = await axios.get(`http://localhost:3001/users/${userId}`)
+    const getUser = async () => {
+      const viewedUserResponse = await axios.get(`http://localhost:3001/users/${userId}`)
 
-  //     const userData = response.data
+      const viewedUserData = viewedUserResponse.data
+      setViewedUser(viewedUserData)
+    }
+    getUser()
 
-  //   }
-  //   getUser()
+    const getUserPosts = async () => {
+      const postsResponse = await axios.get(`http://localhost:3001/userPosts/${userId}`)
 
-  //   const getUserPosts = async () => {
-  //     const response = await axios.get('/posts/:id')
+      const userPosts = postsResponse.data
+      setPosts(userPosts)
+    }
+    getUserPosts()
+  }, [userId])
 
-  //     const userPosts = response.data
-  //   }
-  // })
+  const createNewPost = async (content) => {
+    const newPost = {
+      user_id: loggedInUser._id, 
+      content: content,
+      created_at: new Date(),
+      likes: 0
+    }
+
+    const response = await axios.post(`http://localhost:3001/posts`, newPost)
+    setPosts([response.data.newObject, ...posts])
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createNewPost(postText)
+    setPostText('')
+  }
   
   return (
-    <div>
+    <div className='userProfile'>
       <Header/>
-      <h1>User Profile Home Page</h1>
+      <div className='aboutUser'>
+        <h1>About {viewedUser.firstname}</h1>
+        <h2>{viewedUser.age} Years Old</h2>
+        <h2>Lives in {viewedUser.location}</h2>
+      </div>
+      <form className='createPost' onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          value={postText} 
+          onChange={(e) => setPostText(e.target.value)}
+          placeholder='Write your next post here'
+        />
+        <button className='postButton' type='submit'>Post</button>
+      </form>
+      <div className='posts'>
+        {posts.map(post => (
+            <div key={post._id}>
+              <p>{new Date(post.created_at).toLocaleString()}</p>
+              <p>{post.content}</p>
+              <p>Likes: {post.likes}</p>
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
